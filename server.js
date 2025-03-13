8b88de3aa39987d0191f970500e1a7f0353bfb5f
Z30nn <junno.elizer@gmail.com>
2025-03-13 17:03:20 +0800

    included forms feature

--- server.js
+++ server.js
@@ -2,8 +2,44 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const mime = require('mime-types');
+const formidable = require('formidable');
 
 const server = http.createServer((req, res) => {
+    if (req.method.toLowerCase === 'POST') {
+        const form = new formidable.IncomingForm();
+        form.uploadDir = path.join(__dirname, 'uploads');
+        form.keepExtensions = true;
+
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain'});
+                res.end(err);
+                return;
+            }
+
+            const file = files.upload;
+            const allowedTypes = ['image/jpeg', 'image/png', 'application/pdf'];
+
+            if(!allowedTypes.includes(file.mimetype)) {
+                res.writeHead(400, {'Content-Type': 'text/plain'});
+                res.end('Invalid file type');
+                return;
+            }
+
+            const newFilePath = path.join(form.uploadDir, file.name);
+            fs.rename(file.path, newFilePath, (err) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'text/plain'});
+                    res.end(err);
+                    return;
+                }
+
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('File uploaded successfully');
+            })
+        })
+    }
+
     let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
 
     fs.readFile(filePath, (err, content) => {
